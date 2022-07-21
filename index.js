25d6d9475dc80a758f918819d8857e2dd88a070d
Seungbaek Kim <kimv100@gmail.com>
2022-07-21 14:22:06 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/user");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://kimv100:Mongodb100@boilerplate.ojmz0.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   //useNewUrlParse: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err) )
 
 
   app.get('/', (req, res) => {
-  res.send('Hello World!~~~ 안녕하세유~!')
+  res.send('Hello World!~~~ 안녕하세용~^^ 감사합니다!!!')
+})
+
+
+app.post('/register', (req, res) => {
+  // 회원 가입할 때 필요한 정보를 Client에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다.
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err })
+    return res.status(200).json({ success: true })
+  })
 })
 
 app.listen(port, () => {
--- models/user.js
+++ models/user.js
@@ -24,7 +24,7 @@ const userSchema = mongoose.Schema({
   },
   image: String,
   token: {
-    type: toString,
+    type: String,
   },
   tokenExp: {
     type: Number
